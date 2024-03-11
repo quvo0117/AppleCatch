638000c91ca1730167a4cfc6b10307449efd69af
kb7 <kubo20231020@gmail.com>
2024-03-11 14:31:55 +0900

    retouching ItemGenerator

--- Assets/Scripts/ItemGenerator.cs
+++ Assets/Scripts/ItemGenerator.cs
@@ -8,6 +8,14 @@ public class ItemGenerator : MonoBehaviour
     public GameObject bombPrefab;
     float span = 1.0f;
     float delta = 0;
+    int ratio = 2;
+    float speed = -0.03f;
+
+    public void SetParameter(float span, float speed, int ratio) {
+        this.span = span;
+        this.speed = speed;
+        this.ratio = ratio;
+    }
 
     // Update is called once per frame
     void Update()
@@ -15,7 +23,17 @@ public class ItemGenerator : MonoBehaviour
         this.delta += Time.deltaTime;
         if(this.delta > this.span) {
             this.delta = 0;
-            Instantiate(applePrefab);
+            GameObject item;
+            int dice = Random.Range(1, 11);
+            if(dice <= this.ratio) {
+                item = Instantiate(bombPrefab);
+            } else {
+                item = Instantiate(applePrefab);
+            }
+            float x = Random.Range(-1, 2);
+            float z = Random.Range(-1, 2);
+            item.transform.position = new Vector3(x, 4, z);
+            item.GetComponent<ItemController>().dropSpeed = this.speed;
         }
     }
 }
