d82a84b4272341aed1e4744431cfcc3f06e64ad9
kb7 <kubo20231020@gmail.com>
2024-03-12 10:02:59 +0900

    add GameDirector

---
+++ Assets/Scripts/GameDirector.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;  // TextMeshPro を使う時は忘れないように注意!!
+
+public class GameDirector : MonoBehaviour
+{
+    GameObject timerText;
+    float time = 60.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.timerText = GameObject.Find("Time");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.time -= Time.deltaTime;
+        this.timerText.GetComponent<TextMeshProUGUI>().text = this.time.ToString("F1");
+    }
+}
