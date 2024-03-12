1a4ab51f4bf76faad40a20988d2ba30c6f8975b6
kb7 <kubo20231020@gmail.com>
2024-03-12 10:27:11 +0900

    retouch GameDirector and add point

--- Assets/Scripts/GameDirector.cs
+++ Assets/Scripts/GameDirector.cs
@@ -6,12 +6,15 @@ using TMPro;  // TextMeshPro を使う時は忘れないように注意!!
 public class GameDirector : MonoBehaviour
 {
     GameObject timerText;
+    GameObject pointText;
     float time = 60.0f;
+    int point = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         this.timerText = GameObject.Find("Time");
+        this.pointText = GameObject.Find("Point");
     }
 
     // Update is called once per frame
@@ -19,5 +22,13 @@ public class GameDirector : MonoBehaviour
     {
         this.time -= Time.deltaTime;
         this.timerText.GetComponent<TextMeshProUGUI>().text = this.time.ToString("F1");
+        this.pointText.GetComponent<TextMeshProUGUI>().text = this.point.ToString() + " point";
+    }
+
+    public void GetApple() {
+        this.point += 100;
+    }
+    public void GetBomb() {
+        this.point /= 2;
     }
 }
